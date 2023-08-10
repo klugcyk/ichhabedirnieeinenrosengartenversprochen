7ebceb0655845baa6fef09bc6bb2be8401215c7a
klugcyk <x1021122970@outlook.com>
2023-08-10 14:17:21 +0800

    add img project generate

---
+++ projectedImg/projectedImg.hpp
@@ -0,0 +1,46 @@
+﻿/*
+    文件等级：密一
+    author:klug
+    献给我的心上人等待天使的妹妹
+    start:230810
+    last:230810
+*/
+
+#ifndef projectedImg_HPP
+#define projectedImg_HPP
+
+#include <opencv2/opencv.hpp>
+#include "math/geometry.hpp"
+
+namespace projectImg
+{
+
+// 生成投射用的图像
+class geneProjectImg
+{
+public:
+    //functions
+    geneProjectImg();
+    ~geneProjectImg();
+
+public:
+    //variable
+    cv::Mat projectedImg;
+    cv::Mat reprojectedImg;
+    mathGeometry::point2 projectLocation;
+
+protected:
+    //functions
+
+    //variable
+
+private:
+    //functions
+
+    //variable
+
+};
+
+};
+
+#endif // zwei_phase_cal_HPP
